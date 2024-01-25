6ab83081d467d0722fcff9c05310a1ccf48d658b
jamisonrush <103677258+jamisonrush@users.noreply.github.com>
2024-01-24 17:36:17 -0800

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+router.use(cors())
 
 // TODO: Add your routes here
 router.route("/:reviewId").put(controller.update).delete(controller.destroy).all(methodNotAllowed);
