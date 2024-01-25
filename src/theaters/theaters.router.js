01ed94587b4d08f4df932229dababaa78d5f55fc
jamisonrush <103677258+jamisonrush@users.noreply.github.com>
2024-01-24 16:28:40 -0800

    Update theaters.router.js

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,15 +1,11 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-const cors = require("cors");
-
-const corsGet = cors({ methods: "GET" });
 
 // TODO: Add your routes here
 router
   .route("/")
-  .get(corsGet, controller.list)
-  .options(corsGet)
+  .get(controller.list)
   .all(methodNotAllowed);
 
 module.exports = router;
